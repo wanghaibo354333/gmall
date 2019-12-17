3847f526d50733580623efd050d5d49b5ea34f1c
whbglb <wanghaibo@qq.com>
2019-12-17 17:31:40 +0800

    平台属性开发

---
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/AttrController.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.manage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+/**
+ * @author wanghaibo
+ * @create 2019-12-17 17:00
+ */
+@Controller
+@CrossOrigin
+public class AttrController {
+
+
+
+}
--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/CatalogController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/CatalogController.java
@@ -2,6 +2,8 @@ package com.atguigu.gmall.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.PmsBaseCatalog1;
+import com.atguigu.gmall.bean.PmsBaseCatalog2;
+import com.atguigu.gmall.bean.PmsBaseCatalog3;
 import com.atguigu.gmall.service.CatalogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -29,4 +31,17 @@ public class CatalogController {
          return catalog1;
     }
 
+    @RequestMapping("getCatalog2")
+    @ResponseBody
+    public List<PmsBaseCatalog2> getCatalog1(String catalog1Id){
+        List<PmsBaseCatalog2> catalog2 = catalogService.getCatalog2(catalog1Id);
+        return catalog2;
+    }
+
+    @RequestMapping("getCatalog3")
+    @ResponseBody
+    public List<PmsBaseCatalog3> getCatalog2(String catalog2Id) {
+        List<PmsBaseCatalog3> catalog3 = catalogService.getCatalog3(catalog2Id);
+        return catalog3;
+    }
 }
