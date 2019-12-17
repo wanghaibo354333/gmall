71942f6b13d7f70a5b12d74799cb7e6151cf2010
whbglb <wanghaibo@qq.com>
2019-12-17 16:22:17 +0800

    初始项目完整版

--- gmall-user-web/src/main/java/com/atguigu/gmall/manage/GmallUserWebApplication.java
+++ gmall-user-web/src/main/java/com/atguigu/gmall/manage/GmallUserWebApplication.java
@@ -1,4 +1,4 @@
-package com.atguigu.gmall;
+package com.atguigu.gmall.manage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
