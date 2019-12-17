a4557cb750a7e6c780b092bf58f69992f6347406
whbglb <wanghaibo@qq.com>
2019-12-17 17:29:08 +0800

    平台属性列表开发

---
+++ gmall-api/src/main/java/com/atguigu/gmall/service/AttrService.java
@@ -0,0 +1,24 @@
+package com.atguigu.gmall.service;
+
+import com.atguigu.gmall.bean.PmsBaseAttrInfo;
+import com.atguigu.gmall.bean.PmsBaseAttrValue;
+import com.atguigu.gmall.bean.PmsBaseSaleAttr;
+
+import java.util.List;
+
+/**
+ * @author wanghaibo
+ * @create 2019-12-17 17:16
+ */
+public interface AttrService {
+
+    List<PmsBaseAttrInfo> attrInfoList(String catalog3Id);
+
+    String saveAttrInfo(PmsBaseAttrInfo pmsBaseAttrInfo);
+
+    List<PmsBaseAttrValue> getAttrValueList(String attrId);
+
+    List<PmsBaseSaleAttr> baseSaleAttrList();
+
+
+}
