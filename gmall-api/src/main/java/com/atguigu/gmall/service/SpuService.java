362de6599fefbb42c4eb93bf21875a40f8a3c138
whbglb <wanghaibo@qq.com>
2019-12-18 19:00:48 +0800

    spu功能添加

---
+++ gmall-api/src/main/java/com/atguigu/gmall/service/SpuService.java
@@ -0,0 +1,24 @@
+package com.atguigu.gmall.service;
+
+import com.atguigu.gmall.bean.PmsProductImage;
+import com.atguigu.gmall.bean.PmsProductInfo;
+import com.atguigu.gmall.bean.PmsProductSaleAttr;
+
+import java.util.List;
+
+/**
+ * @author wanghaibo
+ * @create 2019-12-18 18:02
+ */
+public interface SpuService {
+
+    List<PmsProductInfo> spuList(String catalog3Id);
+
+    void saveSpuInfo(PmsProductInfo pmsProductInfo);
+
+    List<PmsProductSaleAttr> spuSaleAttrList(String spuId);
+
+    List<PmsProductImage> spuImageList(String spuId);
+
+    List<PmsProductSaleAttr> spuSaleAttrListCheckBySku(String productId,String skuId);
+}
