c99664488ae98e5794d7134a1868cb0fedbc2b7a
whbglb <wanghaibo@qq.com>
2019-12-20 14:37:40 +0800

    spu与sku功能开发

--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
@@ -1,12 +1,14 @@
 package com.atguigu.gmall.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.PmsProductImage;
 import com.atguigu.gmall.bean.PmsProductInfo;
+import com.atguigu.gmall.bean.PmsProductSaleAttr;
+import com.atguigu.gmall.manage.until.PmsUploadUtil;
 import com.atguigu.gmall.service.SpuService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -22,6 +24,18 @@ public class SpuController {
     @Reference
     SpuService spuService;
 
+
+    @RequestMapping("fileUpload")
+    @ResponseBody
+    public String fileUpload(@RequestParam("file") MultipartFile multipartFile){
+        // 将图片或者音视频上传到分布式的文件存储系统
+        // 将图片的存储路径返回给页面
+        String imgUrl = PmsUploadUtil.uploadImage(multipartFile);
+        System.out.println(imgUrl);
+        return imgUrl;
+    }
+    
+
     @RequestMapping("spuList")
     @ResponseBody
     public List<PmsProductInfo> spuList(String catalog3Id){
@@ -31,5 +45,31 @@ public class SpuController {
             productInfo.setSpuName(productInfo.getProductName());
         }
         return pmsProductInfos;
+    }
+
+
+    @RequestMapping("saveSpuInfo")
+    @ResponseBody
+    public String saveSpuInfo(@RequestBody PmsProductInfo pmsProductInfo){
+
+        spuService.saveSpuInfo(pmsProductInfo);
+        return  "success";
+    }
+
+
+    //spuImageList?spuId=76
+    @RequestMapping("spuImageList")
+    @ResponseBody
+    public List<PmsProductImage> spuImageList(String spuId){
+        List<PmsProductImage> productImages = spuService.spuImageList(spuId);
+        return productImages;
+    }
+
+
+    @RequestMapping("spuSaleAttrList")
+    @ResponseBody
+    public List<PmsProductSaleAttr> spuSaleAttrList(String spuId){
+        List<PmsProductSaleAttr> pmsProductSaleAttrs = spuService.spuSaleAttrList(spuId);
+        return pmsProductSaleAttrs;
     }
 }
